6715555672a8b41d3f176aa758f5b7b69bcc36c8
Ros <rospsix@gmail.com>
2023-05-28 17:58:06 +0100

    #2 local class and anonym class with examples and features

--- src/main/java/org/Java8P1/InnerClasses/Main.java
+++ src/main/java/org/Java8P1/InnerClasses/Main.java
@@ -7,6 +7,29 @@ public class Main {
         list.setKey("Key!!!");
         node.setValue("VALUE!!");
         System.out.println(node.toString());
-    }
 
+        // local class це класи які ми можемо створювати в рамках методів
+        class Local {
+            private String type;
+
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+
+            @Override
+            public String toString() {
+                return "Local{" +
+                        "type='" + type + '\'' +
+                        '}';
+            }
+        }
+        Local local = new Local();
+        local.setType("Java");
+        System.out.println(local.toString());
+    }
 }
